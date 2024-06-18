ce32330df3db651b143a3e0a0931d685ba08539d
jackseabolt <johnseabolt@Johns-MacBook-Pro.local>
2024-06-18 16:29:27 -0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import express from 'express'; 
+import cors from 'cors'; 
+
+const app = express(); 
+
+app.use(cors({
+    origin: '*', // Allow all origins
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS', // Allow specific methods
+    allowedHeaders: ['Content-Type', 'Authorization'], // Allow specific headers
+    credentials: true, // Allow credentials
+})); 
+
+app.get('/test', (req, res) => {
+    return res.json({ 
+        status: 'success'
+    })
+}); 
+
+app.listen(4000, () => {
+    console.log('App listening on 4000')
+}); 
